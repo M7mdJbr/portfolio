60858c9897af5953305d615328d961744671cca2
jboori <m7mdjbr1426@gmail.com>
2026-06-30 20:09:39 +0300

    adding cursor

---
+++ app/components/CustomCursor.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
+export default function CustomCursor() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const moveCursor = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", moveCursor);
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+  return (
+    <div
+      style={{
+        transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+      }}
+      className="fixed top-0 left-0 pointer-events-none z-[9999]
+                 transition-transform duration-100 ease-linear will-change-transform
+                 -translate-x-1/2 -translate-y-1/2"
+    >
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10 2 L18 17 L2 17 Z" fill="white" />
+      </svg>
+    </div>
+  );
+}
